bbff68e26141f5ff0fb70ccdce6727530b5c6f4e
AleksandrsKorsunovs <Aleksandrs.Kors@gmail.com>
2016-11-10 10:00:15 +0200

    Description, new block, Zoom
    IR block added, Pop-up description for all blocks and arguments, Slider
    for workspace zooming.

---
+++ Raw/ardublock/src/main/java/com/ardublock/translator/block/sumoboy/DriveForward.java
@@ -0,0 +1,36 @@
+
+package com.ardublock.translator.block.sumoboy;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.NumberBlock;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.BlockException;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class DriveForward extends TranslatorBlock
+{
+    
+    public DriveForward(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		
+		translator.addHeaderFile("SumoBoy.h");		
+		translator.addDefinitionCommand("SumoBoy sumoboy;");
+		translator.addSetupCommand("sumoboy.init();");
+		
+		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+		String left = translatorBlock.toCode();
+		translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
+		String right = translatorBlock.toCode();
+		
+		String ret = "sumoboy.motor.run( 0,0," + left + ");\n";
+                ret += "sumoboy.motor.run( 1,0," + right + ");\n";
+		return  ret;			
+	}
+    
+}
